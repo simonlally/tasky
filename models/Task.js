3cc65d21bc71d649d8b9ea258963c798fbb32dce
Simon Lally <simonlallydev@gmail.com>
2020-04-13 21:09:12 -0400

    Add createdAt field to Task schema

--- models/Task.js
+++ models/Task.js
@@ -3,6 +3,7 @@ const { model, Schema } = require("mongoose");
 const taskSchema = new Schema({
   body: String,
   completed: Boolean,
+  createdAt: String,
   user: {
     type: Schema.Types.ObjectId,
     ref: "users",
