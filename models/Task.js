6a3b6d8217a5dadc94b8dcc7955af74e2da76fa5
Simon Lally <simonlallydev@gmail.com>
2020-04-13 21:05:10 -0400

    Add a user reference to Task model

--- models/Task.js
+++ models/Task.js
@@ -3,6 +3,10 @@ const { model, Schema } = require("mongoose");
 const taskSchema = new Schema({
   body: String,
   completed: Boolean,
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "users",
+  },
 });
 
 module.exports = model("Task", taskSchema);
