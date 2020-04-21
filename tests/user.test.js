04cc767819cc76ffc0dee0de201ab06b604e890b
Simon Lally <simonlallydev@gmail.com>
2020-04-21 17:44:49 -0400

    Add tests for user registration and login

---
+++ tests/user.test.js
@@ -0,0 +1,38 @@
+const axios = require("axios");
+
+describe("user resolvers", () => {
+  test("four required fields for register", async () => {
+    const response = await axios.post("http://localhost:4000", {
+      query: `
+      mutation {
+        register(registerInput: {
+          username: "testing"
+          password: "password"
+          confirmPassword: "password"
+          email: "test@gmail.com"
+        }) {
+          id
+          username
+          email
+          createdAt
+        }
+      }
+        `,
+    });
+
+    expect(response);
+  });
+
+  test("returns an auth token when login is successful", async () => {
+    const response = await axios.post("http://localhost:4000", {
+      query: `
+            mutation {
+                login(username: "simon", password: "") {
+                    token
+                }
+            }
+          `,
+    });
+    expect(response.data.token);
+  });
+});
