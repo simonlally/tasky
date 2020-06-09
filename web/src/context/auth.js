3c3e9131cfc7006f20f3f9aff4cf6c548b479ee4
Simon Lally <simonlallydev@gmail.com>
2020-06-09 11:02:26 -0400

    Implement auth middleware

---
+++ web/src/context/auth.js
@@ -0,0 +1,68 @@
+import React, { useReducer, createContext } from "react";
+import jwtDecoder from "jwt-decode";
+
+const initialState = {
+  user: null
+};
+
+if (localStorage.getItem("jwtToken")) {
+  const decodedToken = jwtDecoder(localStorage.getItem("jwtToken"));
+  if (decodedToken.exp * 1000 < Date.now()) {
+    localStorage.removeItem("jwtToken");
+  } else {
+    initialState.user = decodedToken;
+  }
+}
+
+const AuthContext = createContext({
+  user: null,
+  login: data => {},
+  logout: () => {}
+});
+
+// reducer
+function authReducer(state, action) {
+  switch (action.type) {
+    case "LOGIN":
+      return {
+        ...state,
+        user: action.payload
+      };
+    case "LOGOUT":
+      return {
+        ...state,
+        user: null
+      };
+    default:
+      return state;
+  }
+}
+
+function AuthProvider(props) {
+  const [state, dispatch] = useReducer(authReducer, initialState);
+
+  const login = data => {
+    localStorage.setItem("jwtToken", data.token);
+    dispatch({
+      type: "LOGIN",
+      payload: data
+    });
+  };
+
+  const logout = () => {
+    localStorage.removeItem("jwtToken");
+    dispatch({
+      type: "LOGOUT"
+    });
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{ user: state.user, login, logout }}
+      {...props}
+    />
+  );
+}
+
+export { AuthContext, AuthProvider };
+© 2020 GitHub, Inc.
