55338a2558990ccbe0b71f43ecfef9981b0091f0
Simon Lally <simonlallydev@gmail.com>
2020-06-15 11:42:38 -0400

    Render task body and username in userfeed from getPostsByUser query

--- web/src/components/UserFeed.js
+++ web/src/components/UserFeed.js
@@ -4,16 +4,30 @@ import { useQuery } from "@apollo/react-hooks";
 import { GET_TASKS_BY_USER_QUERY } from "../util/graphql";
 
 const UserFeed = (user) => {
-  const [tasks, setTasks] = useState([]);
   const username = user.user.user.username;
+  let tasks = [];
 
   const { error, loading, data } = useQuery(GET_TASKS_BY_USER_QUERY, {
     variables: { username },
   });
 
-  console.log(data);
+  if (loading) return <div> LOADING </div>;
 
-  return <div>hello</div>;
+  if (data && data.getTasksByUser) {
+    tasks = data.getTasksByUser;
+  }
+
+  return (
+    <div>
+      {tasks.map((task) => (
+        <div key={task.id}>
+          {" "}
+          {task.body}
+          <div> username: {task.username} </div>{" "}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default UserFeed;
