4d61bc6e88b6016e4f9fa6ff41ab098cebfc902e
Simon Lally <simonlallydev@gmail.com>
2020-06-23 12:48:11 -0400

    Add hook to refresh the current page

--- web/src/util/hooks.js
+++ web/src/util/hooks.js
@@ -7,12 +7,17 @@ export const useForm = (callback, initialState = {}) => {
     setValues({ ...values, [e.target.name]: e.target.value });
   };
 
-  const onSubmit = (event) => {
-    event.preventDefault();
+  const refreshPage = () => {
+    window.location.reload(false);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
     callback();
   };
 
   return {
+    refreshPage,
     onChange,
     onSubmit,
     values,
